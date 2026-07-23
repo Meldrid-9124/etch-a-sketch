b3e92930b8be2c69dbd2e7510361841692d065f3
Maarij Moeen <maarijmoeen12345@gmail.com>
2026-07-23 16:50:45 +0500

    make initial file structure

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("#container");
+
+// used 15 here as we need 16x16 divs
+for (let i = 0; i < 15; i++) {
+    for (let j = 0; j < 15; j++) {
+        
+    }
+}
