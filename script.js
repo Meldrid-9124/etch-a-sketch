18658c2a758a6f4dc23130df284cf0321da6aafc
Maarij Moeen <maarijmoeen12345@gmail.com>
2026-07-23 18:11:22 +0500

    Created squares, coloring and basic resetting

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+// initially, there will be 256 squares i.e a grid of 16x16 squares
+let numberOfSquares = 256;
+
 const container = document.querySelector("#container");
 
-// used 15 here as we need 16x16 divs
-for (let i = 0; i < 15; i++) {
-    for (let j = 0; j < 15; j++) {
-        
+// creating the squares
+function createSquares(numberOfSqaures) {
+    for (let i = 0; i < numberOfSquares; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("grid-square");
+        // hovering effect
+        newSquare.addEventListener('mouseenter', () => {
+            newSquare.style.backgroundColor = "red";
+        })
+        container.appendChild(newSquare);
     }
 }
+
+createSquares(numberOfSquares); 
+
+// button handling
+const squaresBtn = document.querySelector("#sqInputBtn");
+squaresBtn.addEventListener('click', () => {
+    do {
+        numberOfSquares = window.prompt("How many squares do you want per side?", numberOfSquares);
+    } while (numberOfSquares > 100 || numberOfSquares < 0);
+    // erase the current squares
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createSquares(numberOfSquares);
+
+})
